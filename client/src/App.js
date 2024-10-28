cf1da0b84e101ca59bc8fe30ad05c8248f699d0f
Haseeb khan <haseeb.khan@ozteckwork.com>
2024-10-28 16:59:03 +0500

    tailwind added

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,10 @@
+import Navbar from "./components/Navbar";
 import logo from "./logo.svg";
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
