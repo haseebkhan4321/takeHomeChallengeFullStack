0de179bf5dee6dcbd84fff6c6845d8db5a734d43
Haseeb Khan <30334800+haseebkhan4321@users.noreply.github.com>
2024-11-01 02:41:02 +0500

    front end in process

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,25 @@
+import { Switch } from "@headlessui/react";
+import Blogs from "./components/Blogs";
 import Navbar from "./components/Navbar";
-import logo from "./logo.svg";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Login from "./pages/login";
+import Signup from "./pages/signup";
+import Profile from "./pages/profile";
+import PageNotFound from "./pages/PageNotFound";
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Blogs />}></Route>
+          <Route path="/login" element={<Login />}></Route>
+          <Route path="/signup" element={<Signup />}></Route>
+          <Route path="/profile" element={<Profile />}></Route>
+          <Route path="/*" element={<PageNotFound />}></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
